42105513900cc1035733c1170e8eae66d6f51a58
jackgreenberg <jgreenberg@rubbl.com>
2024-11-03 01:05:24 -0600

    fixed sizing of the temporary logo, need to remove background and change color and also click on it

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -4,8 +4,8 @@ export default function Header() {
   return (
     <header className="bg-orange-600 text-white p-4 flex items-center justify-between">
       <div className="flex items-center">
-        <img src="/longhorn-rooms-primary.png" alt="Logo" className="h-10 w-10 mr-4" />
-        <h1 className="text-xl font-bold">Longhorn Rooms</h1>
+      <img src="/longhorn-rooms-primary.png" alt="Logo" className="h-10 mr-4" />
+      <h1 className="text-xl font-bold">Longhorn Rooms</h1>
       </div>
       <nav className="flex space-x-4">
         <Link href="/" legacyBehavior>
