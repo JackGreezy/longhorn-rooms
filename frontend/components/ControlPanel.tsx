7346159e0f75661c7b2bd821612aa43a65fa7629
Kickblip <wyatt.zilker@gmail.com>
2024-11-03 00:24:12 -0500

    downgrade to react 18, shadcn sheet

--- frontend/components/ControlPanel.tsx
+++ frontend/components/ControlPanel.tsx
@@ -9,13 +9,13 @@ export default function ControlPanel({ view, setView }: ControlPanelProps) {
       <h3 className="text-3xl font-semibold mb-4">Control Panel</h3>
       <button
         onClick={() => setView("map")}
-        className={`w-full mb-2 px-4 py-2 text-white rounded ${view === "map" ? "bg-blue-600" : "bg-blue-400"}`}
+        className={`w-full mb-2 px-4 py-2 text-white rounded ${view === "map" ? "bg-orange-600" : "bg-orange-400"}`}
       >
         Map View
       </button>
       <button
         onClick={() => setView("list")}
-        className={`w-full px-4 py-2 text-white rounded ${view === "list" ? "bg-blue-600" : "bg-blue-400"}`}
+        className={`w-full px-4 py-2 text-white rounded ${view === "list" ? "bg-orange-600" : "bg-orange-400"}`}
       >
         List View
       </button>
